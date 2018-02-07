82c79c7bcf94e833cb2681c53b8d6c0cbbf9d67c
axver <ecimoramora@gmail.com>
2018-02-07 12:06:23 +0700

    Restore dulu

---
+++ weather.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>OpenWeatherMap for Leaflet</title>
+	<meta charset="utf-8" />
+	<link rel="SHORTCUT ICON" HREF="plugin_weather/example/owm.ico">
+	<!-- <link rel="stylesheet" type="text/css" href="../plugin_weather/leaflet/leaflet.css" />
+	<link rel="stylesheet" type="text/css" href="../plugin_weather/leaflet-openweathermap.css" />
+	<link rel="stylesheet" type="text/css" href="../plugin_weather/files/map.css" /> -->
+	<script src="plugin_weather/leaflet/leaflet.js"></script>
+	<script src="plugin_weather/leaflet/Permalink.js"></script>
+	<script src="plugin_weather/leaflet/Permalink.Layer.js"></script>
+	<script src="plugin_weather/leaflet/Permalink.Overlay.js"></script>
+	<script src="plugin_weather/leaflet-openweathermap.js"></script>
+	<link rel="stylesheet" type="text/css" href="plugin_weather/leaflet/leaflet-languageselector.css" />
+	<script src="plugin_weather/leaflet/leaflet-languageselector.js"></script>
+	<script src="plugin_weather/files/map_i18n.js"></script>
+	<script src="plugin_weather/files/map.js"></script>
+</head>
+<body>
+	<div id="map"></div>
+	<script>
+		initMap();
+	</script>
+</body>
+</html>
