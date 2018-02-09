79203bf5cdc6cfe54dcd72134dfc8fee8d04b323
axver <ecimoramora@gmail.com>
2018-02-09 14:34:25 +0700

    Project last

--- loginpage.php
+++ loginpage.php
@@ -3,15 +3,15 @@
 	<head>
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		
+
 		<!-- jQuery library -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		
+
 		<!-- Latest compiled JavaScript -->
-		
+
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<script>
-			
+
 			function redirectdaftar(){
 				window.location.href = "../magang_1/registerpage.php";
 			}
@@ -20,23 +20,23 @@
         <meta charset="utf-8">
 		<title>Login</title>
 		<style media="screen">
-			
+
 		</style>
 	</head>
-	
+
 	<body style="background-color: #ffffff; margin-top: 100px" >
-		
+
 		<div class="container" align="center" >
 			<div class="row" style="position: center">
 				<div class="col-sm-3">
-					
+
 				</div>
 				<div class="col-sm-3" >
 				    <div id="login"  class="panel panel-info" style=" margin-left:90px; width:400px;">
                         <div class="panel-head" style="background-color: #b38f00; ">
-                            <img class="img-responsive" style="width:170px; height:50px;" href="index.php" src="../fw_bootstrap/img/3.png" alt="" >
+                            <img class="img-responsive" style="width:170px; height:50px;" href="index.php" src="image/1log.png" alt="" >
                         </div>
-				
+
                         <div style="background-color: lightyellow ; height: 280px;" class="panel-body">
 
                             <form action="sy_proses/pr_login.php" method="post">
@@ -62,4 +62,3 @@
         </div>
     </body>
 </html>
-								
